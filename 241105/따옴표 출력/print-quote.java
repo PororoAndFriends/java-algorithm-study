538ffb12b2b620d6b81d3764c8085661c76acabb
Pororo <130421104+PororoAndFriends@users.noreply.github.com>
2024-11-05 16:14:33 +0900

    Title: 따옴표 출력, Time: 91ms, Memory: 8MB, Status: Passed - CodeTree

--- 241105/따옴표 출력/print-quote.java
+++ 241105/따옴표 출력/print-quote.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-         System.out.print("\"Hello\"");
+         System.out.println("He says \"It's a really simple sentence\".");
     }
 }
