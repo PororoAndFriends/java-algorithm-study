920903ebb07978433b6d5f2b516bc705d11d2f57
Pororo <130421104+PororoAndFriends@users.noreply.github.com>
2024-11-28 17:52:30 +0900

    Title: 재귀 속의 합계, Time: 128ms, Memory: 10MB, Status: Passed - CodeTree

--- 241128/재귀 속의 합계/sum-in-recursive-state.java
+++ 241128/재귀 속의 합계/sum-in-recursive-state.java
@@ -5,9 +5,9 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int num = scanner.nextInt();
         if(num % 2 == 0){
-            System.out.println((1 + num) / 2 * num);
+            System.out.println((1 + num) * num / 2);
         }else{
-            System.out.println((num - 1) / 2 * num + num);
+            System.out.println((num - 1) * num / 2 + num);
         }
     }
 }
