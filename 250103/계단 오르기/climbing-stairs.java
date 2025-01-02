ec0926669219fc78e2ad032c4018517f45251fc6
Pororo <130421104+PororoAndFriends@users.noreply.github.com>
2025-01-03 00:14:51 +0900

    Title: 계단 오르기, Time: 141ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 250103/계단 오르기/climbing-stairs.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        if(n == 2){
+            System.out.println(1);
+            return;
+            
+        }
+
+
+        int arr[] = new int[n+1];
+        for(int i =0; i < arr.length; i++){
+            arr[i] = 0;
+        }
+        arr[0] = 0;
+        arr[2] = 1;
+        arr[3] = 1;
+        for(int i=4; i<arr.length; i++){
+            if(arr[i] == 0){
+                arr[i] = arr[i-2] + arr[i-3];
+            }
+        }
+        System.out.println(arr[n] % 10007);
+    }
+}
