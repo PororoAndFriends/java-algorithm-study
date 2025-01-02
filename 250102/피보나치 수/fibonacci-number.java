4441055ef10b16dc730467156cecf319836b94ab
Pororo <130421104+PororoAndFriends@users.noreply.github.com>
2025-01-02 23:47:46 +0900

    Title: 피보나치 수, Time: 124ms, Memory: 10MB, Status: Runtime Error - CodeTree

---
+++ 250102/피보나치 수/fibonacci-number.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+
+        int[] arr = new int[n+1];
+        arr[1] = 1;
+        arr[2] = 1;
+
+        for(int i=3;i<=n;i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+        System.out.println(arr[n]);
+
+
+
+    }
+}
