141a6ffcd4e74f3edb448081cf21930290337bc5
Pororo <130421104+PororoAndFriends@users.noreply.github.com>
2025-01-03 00:29:56 +0900

    Title: 계단 오르기, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 250103/계단 오르기/climbing-stairs.java
+++ 250103/계단 오르기/climbing-stairs.java
@@ -4,7 +4,7 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        if(n == 2){
+        if(n == 2 || n==3){
             System.out.println(1);
             return;
             
@@ -18,10 +18,6 @@ public class Main {
         arr[3] = 1;
         for(int i=4; i<arr.length; i++){
             arr[i] = arr[i-2] + arr[i-3];
-        }
-        if(arr[n] == 0){
-            System.out.println(0);
-            return;
         }
         System.out.println(arr[n] % 10007);
     }
