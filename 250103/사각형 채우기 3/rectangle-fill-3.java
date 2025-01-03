3782afeb89b6978c5582895115e39fde3fe4b56c
Pororo <130421104+PororoAndFriends@users.noreply.github.com>
2025-01-03 17:04:12 +0900

    Title: 사각형 채우기 3, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

--- 250103/사각형 채우기 3/rectangle-fill-3.java
+++ 250103/사각형 채우기 3/rectangle-fill-3.java
@@ -6,13 +6,10 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
 
-        int arr[] = new int[n+1];
+        long arr[] = new long[n+1];
 
         if(n==1){
             System.out.println(2);
-            return;
-        }else if(n==2){
-            System.out.println(7);
             return;
         }
 
@@ -20,9 +17,8 @@ public class Main {
 
         arr[0] = 1;
         arr[1] = 2;
-        arr[2] = 7;
 
-        for(int i=3;i<n+1;i++){
+        for(int i=2;i<n+1;i++){
             arr[i] = (arr[i-1] * 2 + arr[i-2] * 3) % MOD;
             for(int j=i-3;j>=0;j--){
 
