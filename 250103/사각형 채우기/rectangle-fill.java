5ce6dde9590b7d9888f063137d76affaffc67722
Pororo <130421104+PororoAndFriends@users.noreply.github.com>
2025-01-03 15:39:32 +0900

    Title: 사각형 채우기, Time: 124ms, Memory: 10MB, Status: Passed - CodeTree

--- 250103/사각형 채우기/rectangle-fill.java
+++ 250103/사각형 채우기/rectangle-fill.java
@@ -19,7 +19,7 @@ public class Main {
 
 
         for(int i =3; i < n+1; i++){
-            arr[i] = arr[i-1] + arr[i-2];
+            arr[i] = (arr[i-1] + arr[i-2])%10007;
         }
 
         System.out.println(arr[n]);
